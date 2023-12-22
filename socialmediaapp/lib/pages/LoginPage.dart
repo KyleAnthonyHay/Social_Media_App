3a3210adbca21a6ef8ea4a25b0265fab2fbd49e3
KyleAnthonyHay <haykyle917@gmail.com>
2023-12-22 00:57:24 -0500

    login page started

--- socialmediaapp/lib/pages/LoginPage.dart
+++ socialmediaapp/lib/pages/LoginPage.dart
@@ -5,6 +5,32 @@ class LoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.background,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // logo
+            Icon(
+              Icons.account_circle,
+              size: 100,
+              color: Theme.of(context).colorScheme.inversePrimary,
+            ),
+            //app name
+
+            // email text field
+
+            // password text field
+
+            // forgot password
+
+            // sign in button
+
+            // blurb
+          ],
+        ),
+      ),
+    );
   }
 }
